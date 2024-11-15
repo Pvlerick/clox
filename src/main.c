b85b17e6de62118f07110eea3be29a3aa2acbf5b
Philippe Vlérick <pvlerick@gmail.com>
2024-11-15 10:03:48 +0100

    without OP_NEGATE

--- src/main.c
+++ src/main.c
@@ -7,12 +7,13 @@ int main(int argc, const char *argv[]) {
   Chunk chunk;
   initChunk(&chunk);
 
-  writeConstant(&chunk, 1.2, 123);
-  writeConstant(&chunk, 3.4, 123);
-  writeChunk(&chunk, OP_ADD, 123);
-  writeConstant(&chunk, 5.6, 123);
-  writeChunk(&chunk, OP_DIVIDE, 123);
-  writeChunk(&chunk, OP_NEGATE, 123);
+  writeConstant(&chunk, 4, 123);
+  writeConstant(&chunk, 3, 123);
+  writeChunk(&chunk, OP_SUBTRACT, 123);
+  writeConstant(&chunk, 0, 123);
+  writeConstant(&chunk, 2, 123);
+  writeChunk(&chunk, OP_SUBTRACT, 123);
+  writeChunk(&chunk, OP_MULTIPLY, 123);
 
   writeChunk(&chunk, OP_RETURN, 0);
 
