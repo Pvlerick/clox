3de74da69abdc50b158acfe960116d6dc87408e1
Philippe Vlérick <pvlerick@gmail.com>
2024-11-15 10:05:12 +0100

    without OP_SUBTRACT

--- src/main.c
+++ src/main.c
@@ -9,10 +9,10 @@ int main(int argc, const char *argv[]) {
 
   writeConstant(&chunk, 4, 123);
   writeConstant(&chunk, 3, 123);
-  writeChunk(&chunk, OP_SUBTRACT, 123);
-  writeConstant(&chunk, 0, 123);
+  writeChunk(&chunk, OP_NEGATE, 123);
+  writeChunk(&chunk, OP_ADD, 123);
   writeConstant(&chunk, 2, 123);
-  writeChunk(&chunk, OP_SUBTRACT, 123);
+  writeChunk(&chunk, OP_NEGATE, 123);
   writeChunk(&chunk, OP_MULTIPLY, 123);
 
   writeChunk(&chunk, OP_RETURN, 0);
