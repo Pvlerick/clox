45fc60e7d21b8059eafddecb9feaebd4d41135ef
Philippe Vlérick <pvlerick@gmail.com>
2024-11-29 12:59:02 +0100

    printing and indenting calls

--- src/compiler.c
+++ src/compiler.c
@@ -39,6 +39,100 @@ typedef struct {
 Parser parser;
 Chunk *compilingChunk;
 
+static int depth = 0;
+static void printDepth() {
+  for (int i = 0; i < depth; i++)
+    printf(" ");
+}
+static void incDepth() { depth++; }
+static void decDepth() { depth--; }
+
+static const char *printableTokenType(TokenType type) {
+  switch (type) {
+
+  case TOKEN_LEFT_PAREN:
+    return "TOKEN_LEFT_PAREN";
+  case TOKEN_RIGHT_PAREN:
+    return "TOKEN_RIGHT_PAREN";
+  case TOKEN_LEFT_BRACE:
+    return "TOKEN_LEFT_BRACE";
+  case TOKEN_RIGHT_BRACE:
+    return "TOKEN_RIGHT_BRACE";
+  case TOKEN_COMMA:
+    return "TOKEN_COMMA";
+  case TOKEN_DOT:
+    return "TOKEN_DOT";
+  case TOKEN_MINUS:
+    return "TOKEN_MINUS";
+  case TOKEN_PLUS:
+    return "TOKEN_PLUS";
+  case TOKEN_SEMICOLON:
+    return "TOKEN_SEMICOLON";
+  case TOKEN_SLASH:
+    return "TOKEN_SLASH";
+  case TOKEN_STAR:
+    return "TOKEN_STAR";
+  case TOKEN_BANG:
+    return "TOKEN_BANG";
+  case TOKEN_BANG_EQUAL:
+    return "TOKEN_BANG_EQUAL";
+  case TOKEN_EQUAL:
+    return "TOKEN_EQUAL";
+  case TOKEN_EQUAL_EQUAL:
+    return "TOKEN_EQUAL_EQUAL";
+  case TOKEN_GREATER:
+    return "TOKEN_GREATER";
+  case TOKEN_GREATER_EQUAL:
+    return "TOKEN_GREATER_EQUAL";
+  case TOKEN_LESS:
+    return "TOKEN_LESS";
+  case TOKEN_LESS_EQUAL:
+    return "TOKEN_LESS_EQUAL";
+  case TOKEN_IDENTIFIER:
+    return "TOKEN_IDENTIFIER";
+  case TOKEN_STRING:
+    return "TOKEN_STRING";
+  case TOKEN_NUMBER:
+    return "TOKEN_NUMBER";
+  case TOKEN_AND:
+    return "TOKEN_AND";
+  case TOKEN_CLASS:
+    return "TOKEN_CLASS";
+  case TOKEN_ELSE:
+    return "TOKEN_ELSE";
+  case TOKEN_FALSE:
+    return "TOKEN_FALSE";
+  case TOKEN_FOR:
+    return "TOKEN_FOR";
+  case TOKEN_FUN:
+    return "TOKEN_FUN";
+  case TOKEN_IF:
+    return "TOKEN_IF";
+  case TOKEN_NIL:
+    return "TOKEN_NIL";
+  case TOKEN_OR:
+    return "TOKEN_OR";
+  case TOKEN_PRINT:
+    return "TOKEN_PRINT";
+  case TOKEN_RETURN:
+    return "TOKEN_RETURN";
+  case TOKEN_SUPER:
+    return "TOKEN_SUPER";
+  case TOKEN_THIS:
+    return "TOKEN_THIS";
+  case TOKEN_TRUE:
+    return "TOKEN_TRUE";
+  case TOKEN_VAR:
+    return "TOKEN_VAR";
+  case TOKEN_WHILE:
+    return "TOKEN_WHILE";
+  case TOKEN_ERROR:
+    return "TOKEN_ERROR";
+  case TOKEN_EOF:
+    return "TOKEN_EOF";
+  }
+}
+
 static Chunk *currentChunk() { return compilingChunk; }
 
 static void errorAt(Token *token, const char *message) {
@@ -113,13 +207,14 @@ static void endCompiler() {
 #endif
 }
 
-static void expression();
 static ParseRule *getRule(TokenType);
 static void parsePrecedence(Precedence);
 
 static void expression() { parsePrecedence(PREC_ASSIGNMENT); }
 
 static void binary() {
+  printf("handler: %s\n", __FUNCTION__);
+
   TokenType operatorType = parser.previous.type;
   ParseRule *rule = getRule(operatorType);
   parsePrecedence((Precedence)rule->precedence + 1);
@@ -143,16 +238,20 @@ static void binary() {
 }
 
 static void grouping() {
+  printf("handler: %s\n ", __FUNCTION__);
   expression();
   consume(TOKEN_RIGHT_PAREN, "Expect ')' after expression.");
 }
 
 static void number() {
+  printf("handler: %s\n", __FUNCTION__);
   double value = strtod(parser.previous.start, nullptr);
   emitConstant(value);
 }
 
 static void unary() {
+  printf("handler: %s\n", __FUNCTION__);
+
   TokenType operatorType = parser.previous.type;
 
   parsePrecedence(PREC_UNARY);
@@ -212,6 +311,10 @@ ParseRule rules[] = {
 static void parsePrecedence(Precedence precedence) {
   advance();
 
+  printDepth();
+  printf("token: %s | precedence: %d | ",
+         printableTokenType(parser.previous.type), precedence);
+
   ParseFn prefixRule = getRule(parser.previous.type)->prefix;
 
   if (prefixRule == nullptr) {
@@ -219,12 +322,22 @@ static void parsePrecedence(Precedence precedence) {
     return;
   }
 
+  incDepth();
   prefixRule();
+  decDepth();
 
   while (precedence <= getRule(parser.current.type)->precedence) {
     advance();
-    ParseFn infifRule = getRule(parser.previous.type)->infix;
-    infifRule();
+
+    printDepth();
+    printf("token: %s | precedence: %d | ",
+           printableTokenType(parser.previous.type), precedence);
+
+    ParseFn infixfRule = getRule(parser.previous.type)->infix;
+
+    incDepth();
+    infixfRule();
+    decDepth();
   }
 }
 
